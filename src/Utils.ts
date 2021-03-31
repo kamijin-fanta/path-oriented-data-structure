3d41fac096ae02bf0308a05358a717e40490245a
kamijin_fanta <kamijin@live.jp>
2021-03-31 17:03:27 +0900

    fix: support for windows, custom delimiter

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,9 +1,20 @@
-import { normalize } from "path";
-
 export const generateKey = (kind: string, name: string): string => {
   return `${kind}:${name}`;
 };
 
 export const split = (p: string, delimiter: string): string[] => {
-  return normalize(p).split(delimiter);
+  const array = [];
+  for (const seg of p.split(delimiter)) {
+    switch (seg) {
+      case ".":
+        break;
+      case "..":
+        array.pop();
+        break;
+      default:
+        array.push(seg);
+        break;
+    }
+  }
+  return array;
 };
